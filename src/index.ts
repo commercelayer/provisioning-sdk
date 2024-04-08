a49c4379b39c1cd1b02b52375dd76a58371fd0b8
Pierluigi Viti <pierluigi@commercelayer.io>
2024-04-08 10:53:12 +0200

    fix: fix package exports

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,14 @@
 // SDK
 export { default, CommerceLayerProvisioning } from './commercelayer'
 
-// Commerce Layer Provisioning client type
-export type { CommerceLayerProvisioningClient } from './commercelayer'
-
 // Commerce Layer Provisioning static functions
 export { CommerceLayerProvisioningStatic } from './static'
+
+
+/** **  TYPES ** **/
+
+// Commerce Layer Provisioning client type
+export type { CommerceLayerProvisioningClient } from './commercelayer'
 
 // Query filter types
 export type { QueryParamsRetrieve, QueryParamsList, QueryParams } from './query'
