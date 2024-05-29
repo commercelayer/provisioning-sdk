18437d0a2d6630e132537f4275176fdb470ae2a3
Pierluigi Viti <pierluigi@commercelayer.io>
2024-05-29 12:21:32 +0200

    feat: add refresh token function

--- specs/commercelayer.spec.ts
+++ specs/commercelayer.spec.ts
@@ -1,7 +1,7 @@
 
-import { CommerceLayerProvisioningClient, CommerceLayerProvisioningStatic } from '../src'
+import { CommerceLayerProvisioning, CommerceLayerProvisioningClient, CommerceLayerProvisioningStatic } from '../src'
 import { getClient } from '../test/common'
-import { RawResponseReader } from '../src/interceptor'
+import getAccessToken from '../test/token'
 
 
 let clp: CommerceLayerProvisioningClient
@@ -43,11 +43,40 @@ describe('SDK:commercelayer suite', () => {
 		if (headers) expect(reader.headers).not.toBeUndefined()
 		else expect(reader.headers).toBeUndefined()
 
-		cli.removeRawResponseReader(reader.id as number)
-		cli.removeRawResponseReader(reader)
-		cli.removeRawResponseReader(0)
-		cli.removeRawResponseReader({ id: 1, ok: true } as RawResponseReader)
+		cli.removeRawResponseReader()
 
+		clp = await getClient()
+
+	})
+
+
+	it('commercelayer.refreshToken', async () => {
+
+		let refreshed = false
+
+		async function refreshToken(old: string): Promise<string> {
+			const token = (await getAccessToken('user')).accessToken
+			refreshed = true
+			return token
+		}
+
+		const domain = process.env.CL_SDK_DOMAIN as string
+		const expiredToken = 'eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCIsImtpZCI6IjliN2JiZmVlMzQzZDVkNDQ5ZGFkODhmMjg0MGEyZTM3YzhkZWFlZTg5NjM4MGQ1ODA2YTc4NWVkMWQ1OTc5ZjAifQ.eyJ1c2VyIjp7ImlkIjoiYUVaa3lTQlhMVyJ9LCJhcHBsaWNhdGlvbiI6eyJpZCI6Ik54RFppeUFBa04iLCJraW5kIjoidXNlciIsInB1YmxpYyI6ZmFsc2V9LCJzY29wZSI6InByb3Zpc2lvbmluZy1hcGkiLCJleHAiOjE3MTI3NTU1NzQsInRlc3QiOmZhbHNlLCJyYW5kIjowLjQwNzEwODk4NDE5ODU4NDU0LCJpYXQiOjE3MTI3NDgzNzQsImlzcyI6Imh0dHBzOi8vY29tbWVyY2VsYXllci5pbyJ9.IXs33dY4PcaedryPyU99kxPQfyVQYcLPwK8GRamVY18KQyiOgCXJbqUMQMfk4WzKaiI3HHwSlbqVXSKe4P3gGHHIUXj1Xdc07VtcS9AZpu6QgMoec1lgeK1pcV41DtWaj-QekN61HFr_oLHTjvlcpO31FJB-Zw50C1trj8gK-dtnYbzH9_AIevWnPMzRGRGq2xcPO7Uw0bEa8pSxkl_gO6e4wM55IPB9IQX86qmW9q_tD7T7IwGEB8mbcwMC7E4YtjlwAzZCXbBuMvFKj5Wma3DwM1Z0-dJAbCw6c-YOzCKvXgnmMYaHgWLnTcpJTk3UpJ_SpZn_kwUa31VFhVP3nupfNTr9bg7Z_kQ9120Vrgq0MMB4wr0c8SARz1pcvvqeU4eUHgamQlugbgoYx8Vm-kxNzcdyT8nRDlAdd1-xglghjLZNcQkxD_rJZWyYd234MXPgpshLYENfkh-QvCxdRjCh_iHazufa3taYcuc4KwQ-Vw-oeiBozNh3JgptjUrd3GMavLpDbXEUa8tH2dz7H2yEjH5vKo5pprno5gKQWnIVO0yHcI32TmWIalV8nouyfvOCrRQaCqJJK7NCtbZZMEVarWP09i6wUy1FKI3rfnBmAIHZxcEn1yOS5ESXfXj8Kt4GVmPG7yIDmCw8P37JAi2xb45oXldzyTyk1jbQMq4'
+
+		const cli = CommerceLayerProvisioning({
+			domain,
+			accessToken: expiredToken,
+			refreshToken
+		})
+
+		expect(cli.currentAccessToken).toBe(expiredToken)
+
+		await cli.memberships.list({ pageSize: 1 })
+
+		expect(refreshed).toBeTruthy()
+		expect(cli.currentAccessToken).toBeDefined()
+		expect(cli.currentAccessToken).not.toBe(expiredToken)
+		
 		clp = await getClient()
 
 	})
--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,5 @@
 import type { ObjectType } from "../src/types"
-// import path from 'path'
+// import path from 'node:path'
 
 
 
@@ -22,11 +22,11 @@ const nestedField = (obj: any, field: string): { key: string, val: any } => {
 	let fp = field
 	if (fp.endsWith('.')) fp = fp.substring(0, fp.length-1)
 
-    const dots = field.split(".")
+  const dots = field.split(".")
 
 	const key = dots[dots.length-1]
 	let val = obj
-    while (dots.length && (val = val[dots.shift() || '']));
+  while (dots.length && (val = val[dots.shift() || '']))
     
 	return { key, val }
 }
@@ -36,12 +36,22 @@ const nestedField = (obj: any, field: string): { key: string, val: any } => {
 const packageInfo = (fields?: string | string[], options?: any): Record<string, any>  => {
 	const pjson = require(path.resolve('./', 'package.json'))
 	return fields? (Array.isArray(fields)? fields : [ fields ]).reduce((info: any, field) => {
-		const nf = nestedField(pjson, field)
-		info[options?.nestedName? nf.key : field] = nf.val
-		return info
+			const nf = nestedField(pjson, field)
+			info[options?.nestedName? nf.key : field] = nf.val
+			return info
 	  }, {}) : pjson
 }
 */
 
 
-export { sleep, sortObjectFields, /* packageInfo */ }
+const isTokenExpired = (token: string): boolean => {
+	try {
+		const tokenData = JSON.parse(atob(token.split('.')[1]))
+		return (((tokenData.exp * 1000) - Date.now()) < 0)
+	} catch (err: any) {
+		return false
+	}
+}
+
+
+export { sleep, sortObjectFields, /* packageInfo */ isTokenExpired }
